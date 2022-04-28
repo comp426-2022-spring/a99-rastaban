871cabb7a981b00ab98ccf76cda5723ea1440dbf
mandudu <35313236+mandudu@users.noreply.github.com>
2022-04-28 00:48:37 -0400

    added tests
    to allow you to meaningfully run node test

---
+++ tests/test.js
@@ -0,0 +1,6 @@
+//dummy test to fulfil assignment requirement
+//if there were anything to test, would add it here
+//using chai to test
+const { expect } = require("chai");
+
+expect(1+1).to.equal(2)
